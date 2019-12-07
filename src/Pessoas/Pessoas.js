9e32e5d52f395f40a2b3fb69f8c0b2d22c93f49a
Heronnes Pereira <heronnestg@hotmail.com>
2019-12-07 13:06:47 -0300

    parte 2

--- src/Pessoas/Pessoas.js
+++ src/Pessoas/Pessoas.js
@@ -1,19 +1,21 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 
 import Pessoa from '../Pessoa/Pessoa'
 
-class Pessoas extends Component {
+class Pessoas extends PureComponent {
    
-  shouldComponentUpdate(nextProps, nextState) {
+  /*shouldComponentUpdate(nextProps, nextState) {
     console.log('[Pessoas.js] shouldComponentUpdate');
-    if (nextProps.pessoas !== this.props.pessoas) {
+    if (nextProps.pessoas !== this.props.pessoas ||
+        nextProps.changed !== this.props.changed || 
+        nextProps.clicked !== this.props.clicked) {
       return true;
     }
     else {
       return false;
     } 
     //return true; 
-  }
+  } */
  
   getSnapshotBeforeUpdate(prevProps, prevState) {
     console.log('[Pessoas.js] getSnapshotBeforeUpdate');
