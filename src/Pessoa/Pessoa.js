78e2bac43d853c72f3e03a47f9a7de405ab85c0b
Heronnes Pereira <heronnestg@hotmail.com>
2019-12-08 12:25:21 -0300

    parte 4

--- src/Pessoa/Pessoa.js
+++ src/Pessoa/Pessoa.js
@@ -2,16 +2,19 @@
 import React, {Component} from 'react';
 
 import classes from './Pessoa.css'
+import Auxiliar from '../hoc/Auxiliar';
 
 class Pessoa extends Component {
 
    render() {
       console.log('[Pessoa.js] rendering...') 
-      return [
-            <p key="i1" onClick={this.props.click}>Eu sou {this.props.nome}, Eu sou uma Pessoa e tenho {this.props.idade} anos !</p>,
-            <p key="i2" >{this.props.children}</p>,
+      return (
+         <Auxiliar>
+            <p key="i1" onClick={this.props.click}>Eu sou {this.props.nome}, Eu sou uma Pessoa e tenho {this.props.idade} anos !</p>
+            <p key="i2" >{this.props.children}</p>
             <input key="i3" type='text' onChange={this.props.changed} value={this.props.nome}></input>
-      ];   
+         </Auxiliar>
+      );
    }
    
 }
