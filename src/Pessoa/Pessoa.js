b51c18ef340b39f36728d26ce35c769b04b9c28d
Heronnes Pereira <heronnestg@hotmail.com>
2019-12-08 12:00:32 -0300

    parte 3

--- src/Pessoa/Pessoa.js
+++ src/Pessoa/Pessoa.js
@@ -7,13 +7,11 @@ class Pessoa extends Component {
 
    render() {
       console.log('[Pessoa.js] rendering...') 
-      return (
-         <div className={classes.Pessoa} >
-            <p onClick={this.props.click}>Eu sou {this.props.nome}, Eu sou uma Pessoa e tenho {this.props.idade} anos !</p>
-            <p>{this.props.children}</p>
-            <input type='text' onChange={this.props.changed} value={this.props.nome}></input>
-         </div>
-      )   
+      return [
+            <p key="i1" onClick={this.props.click}>Eu sou {this.props.nome}, Eu sou uma Pessoa e tenho {this.props.idade} anos !</p>,
+            <p key="i2" >{this.props.children}</p>,
+            <input key="i3" type='text' onChange={this.props.changed} value={this.props.nome}></input>
+      ];   
    }
    
 }
